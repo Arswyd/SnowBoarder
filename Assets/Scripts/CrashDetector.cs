5b6fcee00feba4f679bf9a34fa3568da9d352383
Varsanyi\Varsányi Dániel <vdani94@gmail.com>
2021-07-13 19:39:37 +0200

    Added SoundFX

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,13 +6,15 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
-        [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }
     }
